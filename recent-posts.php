1df1b53e359202ee6c3390df6a660c09e5a24367
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-01-10 02:59:30 +0000

    update for 3.1
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/recent-posts@2111 f53fe46e-8c02-459b-9fdd-93e75771b278

--- recent-posts.php
+++ recent-posts.php
@@ -1,14 +1,15 @@
 <?php
 /*
 Plugin Name: Recent Posts
-Plugin URI: 
+Plugin URI:
 Description:
 Author: Andrew Billits (Incsub)
-Version: 1.0.1
+Version: 2.0
 Author URI:
+WDP ID: 75
 */
 
-/* 
+/*
 Copyright 2007-2009 Incsub (http://incsub.com)
 
 This program is free software; you can redistribute it and/or modify
@@ -32,6 +33,17 @@ display_recent_posts(NUMBER,TITLE_CHARACTERS,CONTENT_CHARACTERS,TITLE_CONTENT_DI
 Ex:
 display_recent_posts(10,40,150,'<br />','<strong>','</strong>','<ul>','</ul>','<li>','</li>','yes','yes',16);
 */
+/* -------------------- Update Notifications Notice -------------------- */
+if ( !function_exists( 'wdp_un_check' ) ) {
+  add_action( 'admin_notices', 'wdp_un_check', 5 );
+  add_action( 'network_admin_notices', 'wdp_un_check', 5 );
+  function wdp_un_check() {
+    if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
+      echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
+  }
+}
+/* --------------------------------------------------------------------- */
+
 //------------------------------------------------------------------------//
 //---Config---------------------------------------------------------------//
 //------------------------------------------------------------------------//
@@ -52,7 +64,7 @@ function display_recent_posts($tmp_number,$tmp_title_characters = 0,$tmp_content
 	global $wpdb;
 	$query = "SELECT * FROM " . $wpdb->base_prefix . "site_posts WHERE blog_public = '1' ORDER BY post_published_stamp DESC LIMIT " . $tmp_number;
 	$tmp_posts = $wpdb->get_results( $query, ARRAY_A );
-	
+
 	if (count($tmp_posts) > 0){
 		echo $tmp_global_before;
 		$default_avatar = get_option('default_avatar');
@@ -62,13 +74,13 @@ function display_recent_posts($tmp_number,$tmp_title_characters = 0,$tmp_content
 				echo $tmp_title_before;
 				if ( $tmp_show_avatars == 'yes' ) {
 					if ( $tmp_title_link == 'no' ) {
-						echo get_avatar( $tmp_post['post_author'], $tmp_avatar_size, $default_avatar) . ' ' . substr($tmp_post['post_title'],0,$tmp_title_characters);				
+						echo get_avatar( $tmp_post['post_author'], $tmp_avatar_size, $default_avatar) . ' ' . substr($tmp_post['post_title'],0,$tmp_title_characters);
 					} else {
 						echo get_avatar( $tmp_post['post_author'], $tmp_avatar_size, $default_avatar) . ' <a href="' . $tmp_post['post_permalink'] . '" style="text-decoration:none;">' . substr($tmp_post['post_title'],0,$tmp_title_characters) . '</a>';
 					}
 				} else {
 					if ( $tmp_title_link == 'no' ) {
-						echo substr($tmp_post['post_title'],0,$tmp_title_characters);				
+						echo substr($tmp_post['post_title'],0,$tmp_title_characters);
 					} else {
 						echo '<a href="' . $tmp_post['post_permalink'] . '" style="text-decoration:none;">' . substr($tmp_post['post_title'],0,$tmp_title_characters) . '</a>';
 					}
