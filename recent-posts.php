509669ab4d961884b6508089a06d12fc9cacef81
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-05-11 20:58:12 +0000

    upgraded to 2.1 Added in posttype query parameter Added shortcode
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/recent-posts@3398 f53fe46e-8c02-459b-9fdd-93e75771b278

--- recent-posts.php
+++ recent-posts.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: Recent Posts
 Plugin URI:
-Description:
+Description: Recent posts function and shortcode
 Author: Andrew Billits (Incsub)
-Version: 2.0
+Version: 2.1
 Author URI:
 WDP ID: 75
 */
@@ -28,10 +28,10 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 /*
 Usage:
-display_recent_posts(NUMBER,TITLE_CHARACTERS,CONTENT_CHARACTERS,TITLE_CONTENT_DIVIDER,TITLE_BEFORE,TITLE_AFTER,GLOBAL_BEFORE,GLOBAL_AFTER,BEFORE,AFTER,TITLE_LINK,SHOW_AVATARS,AVATAR_SIZE);
+display_recent_posts(NUMBER,TITLE_CHARACTERS,CONTENT_CHARACTERS,TITLE_CONTENT_DIVIDER,TITLE_BEFORE,TITLE_AFTER,GLOBAL_BEFORE,GLOBAL_AFTER,BEFORE,AFTER,TITLE_LINK,SHOW_AVATARS,AVATAR_SIZE,POSTTYPE, ECHO);
 
 Ex:
-display_recent_posts(10,40,150,'<br />','<strong>','</strong>','<ul>','</ul>','<li>','</li>','yes','yes',16);
+display_recent_posts(10,40,150,'<br />','<strong>','</strong>','<ul>','</ul>','<li>','</li>','yes','yes',16, 'post', true);
 */
 /* -------------------- Update Notifications Notice -------------------- */
 if ( !function_exists( 'wdp_un_check' ) ) {
@@ -60,42 +60,83 @@ if ( !function_exists( 'wdp_un_check' ) ) {
 //---Output Functions-----------------------------------------------------//
 //------------------------------------------------------------------------//
 
-function display_recent_posts($tmp_number,$tmp_title_characters = 0,$tmp_content_characters = 0,$tmp_title_content_divider = '<br />',$tmp_title_before,$tmp_title_after,$tmp_global_before,$tmp_global_after,$tmp_before,$tmp_after,$tmp_title_link = 'no',$tmp_show_avatars = 'yes', $tmp_avatar_size = 16){
+function display_recent_posts($tmp_number,$tmp_title_characters = 0,$tmp_content_characters = 0,$tmp_title_content_divider = '<br />',$tmp_title_before,$tmp_title_after,$tmp_global_before,$tmp_global_after,$tmp_before,$tmp_after,$tmp_title_link = 'no',$tmp_show_avatars = 'yes', $tmp_avatar_size = 16, $posttype = 'post', $output = true){
 	global $wpdb;
-	$query = "SELECT * FROM " . $wpdb->base_prefix . "site_posts WHERE blog_public = '1' ORDER BY post_published_stamp DESC LIMIT " . $tmp_number;
+	$query = $wpdb->prepare("SELECT * FROM " . $wpdb->base_prefix . "site_posts WHERE blog_public = '1' AND post_type = %s ORDER BY post_published_stamp DESC LIMIT %d", $posttype, $tmp_number);
 	$tmp_posts = $wpdb->get_results( $query, ARRAY_A );
 
+	$html = '';
+
 	if (count($tmp_posts) > 0){
-		echo $tmp_global_before;
+		$html .= $tmp_global_before;
+
 		$default_avatar = get_option('default_avatar');
 		foreach ($tmp_posts as $tmp_post){
-			echo $tmp_before;
+
+			$html .= $tmp_before;
 			if ( $tmp_title_characters > 0 ) {
-				echo $tmp_title_before;
+				$html .= $tmp_title_before;
 				if ( $tmp_show_avatars == 'yes' ) {
 					if ( $tmp_title_link == 'no' ) {
-						echo get_avatar( $tmp_post['post_author'], $tmp_avatar_size, $default_avatar) . ' ' . substr($tmp_post['post_title'],0,$tmp_title_characters);
+						$html .= get_avatar( $tmp_post['post_author'], $tmp_avatar_size, $default_avatar) . ' ' . substr($tmp_post['post_title'],0,$tmp_title_characters);
 					} else {
-						echo get_avatar( $tmp_post['post_author'], $tmp_avatar_size, $default_avatar) . ' <a href="' . $tmp_post['post_permalink'] . '" style="text-decoration:none;">' . substr($tmp_post['post_title'],0,$tmp_title_characters) . '</a>';
+						$html .= get_avatar( $tmp_post['post_author'], $tmp_avatar_size, $default_avatar) . ' <a href="' . $tmp_post['post_permalink'] . '" style="text-decoration:none;">' . substr($tmp_post['post_title'],0,$tmp_title_characters) . '</a>';
 					}
 				} else {
 					if ( $tmp_title_link == 'no' ) {
-						echo substr($tmp_post['post_title'],0,$tmp_title_characters);
+						$html .= substr($tmp_post['post_title'],0,$tmp_title_characters);
 					} else {
-						echo '<a href="' . $tmp_post['post_permalink'] . '" style="text-decoration:none;">' . substr($tmp_post['post_title'],0,$tmp_title_characters) . '</a>';
+						$html .= '<a href="' . $tmp_post['post_permalink'] . '" style="text-decoration:none;">' . substr($tmp_post['post_title'],0,$tmp_title_characters) . '</a>';
 					}
 				}
-				echo $tmp_title_after;
+				$html .= $tmp_title_after;
 			}
-			echo $tmp_title_content_divider;
+			$html .= $tmp_title_content_divider;
 			if ( $tmp_content_characters > 0 ) {
-				echo substr($tmp_post['post_content_stripped'],0,$tmp_content_characters);
+				$html .= substr($tmp_post['post_content_stripped'],0,$tmp_content_characters);
 			}
-			echo $tmp_after;
+			$html .= $tmp_after;
 		}
-		echo $tmp_global_after;
+		$html .= $tmp_global_after;
+
 	}
+
+	if($output) {
+		echo $html;
+	} else {
+		return $html;
+	}
+
 }
+
+function display_recent_posts_shortcode($atts, $content = null, $code = "") {
+
+	$defaults = array(	'number'	=>	5,
+						'title_characters' => 250,
+						'content_characters' => 0,
+						'title_content_divider' => '<br />',
+						'title_before'	=>	'',
+						'title_after'	=>	'',
+						'global_before'	=>	'<ul>',
+						'global_after'	=>	'</ul>',
+						'before'	=>	'<li>',
+						'after'	=>	'</li>',
+						'title_link' => 'yes',
+						'show_avatars' => 'no',
+						'avatar_size' => 16,
+						'posttype' => 'post'
+					);
+
+	extract(shortcode_atts($defaults, $atts));
+
+	$html = '';
+
+	$html .= display_recent_posts( $number, $title_characters, $content_characters, $title_content_divider, $title_before, $title_after, $global_before, $global_after, $before, $after, $title_link, $show_avatars, $avatar_size, $posttype, false);
+
+	return $html;
+
+}
+add_shortcode( 'globalrecentposts', 'display_recent_posts_shortcode');
 
 
 //------------------------------------------------------------------------//
